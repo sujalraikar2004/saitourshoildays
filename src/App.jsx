284ca7257779dfe717a0a07a4ad589cdbcc5b721
sujalraikar2004 <sujalraikar84@gmail.com>
2025-03-03 19:44:45 +0530

    scroller added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
-
+// import Header from './components/Header/Header'
 import './App.css'
 import { Home } from './pages/Home/Home'
 import { About } from './pages/About/About.jsx'
 import { Contact } from './pages/Contact/Contact'
 import TourPackages from './pages/TourPackages/TourPackages.jsx'
-import { CruiseBooking } from './pages/Services/CruiseBooking.jsx'
+import { CruiseBooking } from './pages/Services/CruiseBooking'
 import { AirlineTicketing } from './pages/Services/AirlineTicketing'
 import { HotelBooking } from './pages/Services/HotelBooking'
 import { CarRental } from './pages/Services/CarRental'
@@ -14,10 +14,13 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import TourDetail from './pages/TourDetail/TourDetail.jsx'
 import DomasticTour from './pages/TourByDestination/DomasticTour.jsx'
 import International from './pages/TourByDestination/International.jsx'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+
 function App() {
   return (
     <div className="min-h-screen relative pb-[200px]">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -28,7 +31,8 @@ function App() {
           <Route path="/hotel-booking" element={<HotelBooking />} />
           <Route path="/car-rental" element={<CarRental />} />          
           <Route path="/domastic_tour" element={<DomasticTour />} />
-          <Route path="/international_tour" element={<International />} />
+          <Route path="/international" element={<International />} />
+          <Route path="/services/cruise-booking" element={<CruiseBooking />} />
           <Route path="/detail/:packageId"   element={<TourDetail/>}/>
         </Routes>
       </BrowserRouter>
